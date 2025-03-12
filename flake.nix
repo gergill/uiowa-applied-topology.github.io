82b97f5f898630e1dc0e579b90f4a23a7d98de6a
Ethan Rooke <ethan@roo.ke>
2025-03-12 17:56:10 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "UIowa Applied Topology Website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.hugo
+        pkgs.just
+      ];
+
+      shellHook = ''
+        eval "$(just --completions bash)"
+      '';
+    };
+  };
+}
